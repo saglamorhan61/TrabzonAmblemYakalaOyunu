c368cd9a53c4a27e88da9a3d83f768b119cd9700
ORHAN <saglamorhan.61@gmail.com>
2021-02-17 19:00:11 +0300

    Skoru arttir yapildi.

--- app/src/main/java/com/orhansaglam/trabzonamblemyakalaoyunu/MainActivity.kt
+++ app/src/main/java/com/orhansaglam/trabzonamblemyakalaoyunu/MainActivity.kt
@@ -3,17 +3,21 @@ package com.orhansaglam.trabzonamblemyakalaoyunu
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var skor = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        
 
     }
 
     fun skoruArttir(view : View){
-
+        skor = skor + 1
+        skorText.setText("Skor: $skor")
 
     }
 }
