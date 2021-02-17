e2d49801395fc2492c3736429c2bcae3294c93f3
ORHAN <saglamorhan.61@gmail.com>
2021-02-17 19:20:05 +0300

    Uyari mesajlari yapildi

--- app/src/main/java/com/orhansaglam/trabzonamblemyakalaoyunu/MainActivity.kt
+++ app/src/main/java/com/orhansaglam/trabzonamblemyakalaoyunu/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.orhansaglam.trabzonamblemyakalaoyunu
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +24,25 @@ class MainActivity : AppCompatActivity() {
 
             override fun onFinish() {
                 zamanText.setText("Zaman: 0")
+
+                //Uyari Mesaji
+                val uyariMesaji = android.app.AlertDialog.Builder(this@MainActivity)
+                uyariMesaji.setTitle("Oyun Bitti.")
+                uyariMesaji.setMessage("Tekrar oynamak ister misiniz?")
+                uyariMesaji.setPositiveButton("Evet") { dialog, which ->
+
+                    val intent = Intent(this@MainActivity,MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
+
+                }
+                uyariMesaji.setNegativeButton("Hayır", object : DialogInterface.OnClickListener{
+                    override fun onClick(dialog: DialogInterface?, which: Int) {
+                        Toast.makeText(this@MainActivity,"Oyun Bitti.",Toast.LENGTH_LONG).show()
+                    }
+
+                })
+                uyariMesaji.show()
             }
 
         }.start()
